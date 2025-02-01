a928238bcdd1ed71aab09d1d726ed7eacefe7404
AmaurySilverio <amaurycodes@gmail.com>
2025-02-01 12:35:18 -0500

    updated not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,19 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import "./NotFound.css";
 const NotFound = () => {
-  return <div>404 Not Found</div>;
+  return (
+    <>
+      <Navbar />
+      <div className="not-found-container">
+        <p>
+          We can't find the page you are looking for. Click the home button
+          above to go back to Penny's Party Supplies!
+        </p>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default NotFound;
